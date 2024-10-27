a3b319b3be8c08fe4da9d7dc62e3114123f49258
bouzianes <fcbdreams1991@gmail.com>
2024-10-27 09:27:52 -0400

    async

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-const _ = require('lodash');
+const {readFile, writeFile} = require('fs').promises;
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile)
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err) {
+//                 reject(err)
+//             }else{
+//                 resolve(data)
+//             }
+        
+//         })
 
-const items = [1,[2, [3, [4]]]];
+//     })
+//}
+// getText('./content/first.txt')
+//        .then((result) =>console.log(result))
+//        .catch((err) => {
+//             console.log(err)
+//         })
+const start = async() => {
+    try{
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+       await writeFile('./content/result-mind.txt', `this is Awsome: ${first} ${second}`)
+        console.log(first, second)
+    }
+    catch(err){
+          console.log(err)
+    }
+    
+}
 
-const newItems = _.flattenDeep(items);
-
-console.log(newItems)
+start()
